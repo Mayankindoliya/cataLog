da798fbd985dc7d4de9e6b41ac57c3d018ce05a7
Mayank <mayankindoliya@gmail.com>
2023-04-23 13:21:01 +0530

    Database connected to the server!!

--- server.js
+++ server.js
@@ -2,6 +2,17 @@ const express = require('express');
 
 const app = express();
 
-app.listen(3000, () => {
-  console.log("Server is Ready(((())))!!!")
-})
+const mongoose = require('mongoose');
+
+mongoose.connect("mongodb+srv://mayankindoliya:kiana@cluster0.mfofsan.mongodb.net/catalog?retryWrites=true&w=majority")
+.then(() => {
+  console.log("Database connected!!!")
+  app.listen(3000, () => {
+    console.log("Server is Ready(((())))!!!")
+  })
+}) 
+.catch((err) => {
+  console.log(err)
+});
+
+
